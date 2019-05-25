f3601953044cd2d1b2c410cea81a8f46ab317cb5
niyueyeee <wwy2387090@gmail.com>
2019-05-25 18:23:17 +0800

    5.25

--- pinyougou/pinyougou_web_portal/src/main/webapp/js/controller/payController.js
+++ pinyougou/pinyougou_web_portal/src/main/webapp/js/controller/payController.js
@@ -1,20 +1,21 @@
-app.controller('payController', function ($scope, $location, payService) {
+app.controller('payController' ,function($scope ,$location,payService){
 
-
-    $scope.createNative = function () {
+    //生成二维码
+    // 订单ID (支付表的支付ID)  支付金额  二维码的Value值  code_url
+    $scope.createNative=function(){
         payService.createNative().success(
-            function (response) {
+            function(response){//Map
 
                 //显示订单号和金额
-                $scope.money = (response.total_fee / 100).toFixed(2);
-                $scope.out_trade_no = response.out_trade_no;
+                $scope.money= (response.total_fee/100).toFixed(2);
+                $scope.out_trade_no=response.out_trade_no;
 
                 //生成二维码
-                var qr = new QRious({
-                    element: document.getElementById('qrious'),
-                    size: 250,
-                    value: response.code_url,
-                    level: 'H'
+                var qr=new QRious({
+                    element:document.getElementById('qrious'),
+                    size:250,
+                    value:response.code_url,
+                    level:'H'
                 });
 
                 queryPayStatus();//调用查询
@@ -24,24 +25,29 @@ app.controller('payController', function ($scope, $location, payService) {
     }
 
     //调用查询
-    queryPayStatus = function () {
+    queryPayStatus=function(){
+        //调用 查询时  微信支付系统  查询哪个订单的状态(未支付  已支付
         payService.queryPayStatus($scope.out_trade_no).success(
-            function (response) {
-                if (response.success) {
-                    location.href = "paysuccess.html#?money=" + $scope.money;
-                } else {
-                    if (response.message == '二维码超时') {
+            function(response){
+                if(response.flag){
+                    location.href="paysuccess.html#?money="+$scope.money;
+                }else{
+
+                    if(response.message=='二维码超时' || response.message=='支付超时'){
                         $scope.createNative();//重新生成二维码
-                    } else {
-                        location.href = "payfail.html";
+
+                    }else{
+                        alert(response.message);
+                        location.href="payfail.html";
                     }
+
                 }
             }
         );
     }
 
     //获取金额
-    $scope.getMoney = function () {
+    $scope.getMoney=function(){
         return $location.search()['money'];
     }
 
